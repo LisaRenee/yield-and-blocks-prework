4afe137a66bec79d0486ea1a0ccb29e5c8e7d0c5
Lisa Scott <lrscot@aol.com>
2019-02-14 22:08:28 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,7 +1,8 @@
 def hello_t(names)
   names.each do |name|
-    if name.start_with?("T")
-      puts "Hi, #{name}"
+  #  if name.start_with?("T")
+  #    puts "Hi, #{name}"
+  puts name
     end
   end
 end
