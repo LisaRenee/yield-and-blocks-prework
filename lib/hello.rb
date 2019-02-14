0f3d46a6f29241c6508fe2729fd27d869383ab77
Lisa Scott <lrscot@aol.com>
2019-02-14 22:15:38 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,12 +1,12 @@
 def hello_t(names)
   names.each do |name|
-    if block_given? 
-      yield name 
-    else 
+    if block_given?
+      yield name
+    else
       puts "Hey no block was given!"
-    end 
-  
-  
+    end
+
+
 #  puts name.upcase
   #  end
   end
